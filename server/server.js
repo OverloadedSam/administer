67088aecc7e4d8bd144d46df64d235e0275f4d75
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-04 16:31:55 +0530

    Setup routes and basic configuration at backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
 
 /* CONFIGURATION */
 const server = express();
 dotenv.config();
+server.use(express.json());
+server.use(helmet());
+server.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+server.use(morgan('common'));
+server.use(cors());
+connectDB();
+
+/* ROUTES */
+server.use('/client', clientRoutes);
+server.use('/general', generalRoutes);
+server.use('/management', managementRoutes);
+server.use('/sales', salesRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
