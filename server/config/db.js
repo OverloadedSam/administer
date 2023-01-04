7022de74c859ccd083ab941c4e16f2f69b8173ec
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-04 23:08:25 +0530

    Add dummy data for database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+// import User from '../models/User.js';
+// import { dataUser } from '../data/index.js';
 
 const connectDB = async () => {
   const URI = process.env.DB_CONNECTION_STRING;
@@ -12,6 +14,9 @@ const connectDB = async () => {
     mongoose.set('strictQuery', true);
     await mongoose.connect(URI, options);
     console.log('SUCCESSFULLY CONNECTED TO DB!');
+
+    /* INSERT ONLY FOR 1ST TIME WHEN SETTING UP DEV SERVER */
+    // User.insertMany(dataUser);
   } catch (error) {
     console.log('CONNECTION FAILED TO DB!');
     console.log(error);
