c60a9641531ebd5b1ebed6d4a84b7c2785ac09df
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-04 17:34:40 +0530

    Create material UI theme and add it to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
+import { useMemo } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { createTheme } from '@mui/material/styles';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { themeSettings } from './theme';
+
 const App = () => {
-  return <div className='app'>App</div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className='app'>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+        </ThemeProvider>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import store from './state/store';
 import { Provider } from 'react-redux';
 import App from './App';
+import './css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
