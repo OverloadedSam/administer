f34994d1ae1c8e7bb547ce49666ef3d2bb114d6c
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-04 23:18:35 +0530

    Add express error handler

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import errorHandler from './middlewares/errorHandler.js';
 import clientRoutes from './routes/client.js';
 import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
@@ -24,6 +25,9 @@ server.use('/client', clientRoutes);
 server.use('/general', generalRoutes);
 server.use('/management', managementRoutes);
 server.use('/sales', salesRoutes);
+
+/* CUSTOM ERROR HANDLER */
+server.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
