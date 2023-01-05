f5af3da9ef3e47952accbdb16da359b44420855d
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-05 19:10:16 +0530

    Build scene to show customers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import { themeSettings } from './theme';
 import Layout from './scenes/layout';
 import Dashboard from './scenes/dashboard';
 import Products from './scenes/products';
+import Customers from './scenes/customers';
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -27,6 +28,7 @@ const App = () => {
               <Route path='/' element={<Navigate to='/dashboard' replace />} />
               <Route path='/dashboard' element={<Dashboard />} />
               <Route path='/products' element={<Products />} />
+              <Route path='/customers' element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
---
+++ client/src/scenes/customers/index.jsx
@@ -0,0 +1,93 @@
+import useTheme from '@mui/material/styles/useTheme';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import Loader from '../../components/Loader';
+import Alert from '../../components/Alert';
+import Header from '../../components/Header';
+import { useGetCustomersQuery } from '../../state/apiSlice';
+
+const columns = [
+  { field: '_id', headerName: 'ID', flex: 1 },
+  { field: 'name', headerName: 'Name', flex: 0.5 },
+  { field: 'email', headerName: 'E-mail', flex: 1 },
+  {
+    field: 'phoneNumber',
+    headerName: 'Phone Number',
+    renderCell: (params) =>
+      params.value.replace(/^(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'),
+    flex: 1,
+  },
+  {
+    field: 'country',
+    headerName: 'Country',
+    flex: 0.4,
+  },
+  {
+    field: 'occupation',
+    headerName: 'Occupation',
+    flex: 1,
+  },
+  {
+    field: 'role',
+    headerName: 'Role',
+    flex: 0.5,
+  },
+];
+
+const Customers = () => {
+  const theme = useTheme();
+  const { data, isLoading, isError } = useGetCustomersQuery();
+
+  return (
+    <Box m='1.5rem 2.5rem'>
+      <Header title='Customers' subtitle='List of customers' />
+      {data ? (
+        <Box
+          mt={3}
+          height='75vh'
+          sx={{
+            '& .MuiDataGrid-root': {
+              border: 'none',
+            },
+            '& .MuiDataGrid-cell': {
+              borderBottom: 'none',
+            },
+            '& .MuiDataGrid-columnHeaders': {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: 'none',
+            },
+            '& .MuiDataGrid-virtualScroller': {
+              backgroundColor: theme.palette.primary.light,
+            },
+            '& .MuiDataGrid-footerContainer': {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: 'none',
+            },
+            '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            loading={isLoading || !data}
+            columns={columns}
+            rows={data.data || []}
+            getRowId={(row) => row._id}
+          />
+        </Box>
+      ) : isLoading ? (
+        <Loader mt={5} />
+      ) : isError ? (
+        <Alert
+          variant='outline'
+          mt={4}
+          message={'An error ocurred. Please try again later.'}
+        />
+      ) : null}
+    </Box>
+  );
+};
+
+export default Customers;
