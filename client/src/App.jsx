e77dc2b951ffd20026adbac9c4808cba88ea20d3
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-06 18:49:01 +0530

    Build scene to get monthly sales overview

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import Transactions from './scenes/transactions';
 import Geography from './scenes/geography';
 import Overview from './scenes/overview';
 import Daily from './scenes/daily';
+import Monthly from './scenes/monthly';
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -37,6 +38,7 @@ const App = () => {
               <Route path='/geography' element={<Geography />} />
               <Route path='/overview' element={<Overview />} />
               <Route path='/daily' element={<Daily />} />
+              <Route path='/monthly' element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
