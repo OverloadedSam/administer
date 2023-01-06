b58cd093161691574f4d482c1ad1859985db7a49
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-06 19:25:44 +0530

    Build scene to show breakdown of sales by categories

---
+++ client/src/scenes/breakdown/index.jsx
@@ -0,0 +1,15 @@
+import BreakdownChart from '../../components/BreakdownChart';
+import Box from '@mui/material/Box';
+import Header from '../../components/Header';
+
+const Breakdown = () => {
+  return (
+    <Box m='1.5rem 2.5rem'>
+      <Header title='Breakdown' subtitle='Breakdown of Sales by Category' />
+      <Box mt='40px' height='75vh'>
+        <BreakdownChart />
+      </Box>
+    </Box>
+  );
+};
+export default Breakdown;
