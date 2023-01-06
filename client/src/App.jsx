f7935d06cad897f96d0a2513f88679c833c3decc
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-06 19:48:28 +0530

    Build scene to get list of admins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,7 @@ import Overview from './scenes/overview';
 import Daily from './scenes/daily';
 import Monthly from './scenes/monthly';
 import Breakdown from './scenes/breakdown';
+import Admin from './scenes/admin';
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -41,6 +42,7 @@ const App = () => {
               <Route path='/daily' element={<Daily />} />
               <Route path='/monthly' element={<Monthly />} />
               <Route path='/breakdown' element={<Breakdown />} />
+              <Route path='/admin' element={<Admin />} />
             </Route>
           </Routes>
         </ThemeProvider>
---
+++ client/src/scenes/admin/index.jsx
@@ -0,0 +1,111 @@
+import { DataGrid } from '@mui/x-data-grid';
+import useTheme from '@mui/material/styles/useTheme';
+import Box from '@mui/material/Box';
+import Loader from '../../components/Loader';
+import Alert from '../../components/Alert';
+import Header from '../../components/Header';
+import CustomColumnMenu from '../../components/DataGridCustomColumnMenu';
+import { useGetAdminsQuery } from '../../state/apiSlice';
+
+const columns = [
+  {
+    field: '_id',
+    headerName: 'ID',
+    flex: 1,
+  },
+  {
+    field: 'name',
+    headerName: 'Name',
+    flex: 0.5,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    flex: 1,
+  },
+  {
+    field: 'phoneNumber',
+    headerName: 'Phone Number',
+    flex: 0.5,
+    renderCell: (params) => {
+      return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+    },
+  },
+  {
+    field: 'country',
+    headerName: 'Country',
+    align: 'center',
+    flex: 0.4,
+  },
+  {
+    field: 'occupation',
+    headerName: 'Occupation',
+    flex: 1,
+  },
+  {
+    field: 'role',
+    headerName: 'Role',
+    flex: 0.5,
+  },
+];
+
+const Admin = () => {
+  const theme = useTheme();
+  const { data, isLoading, isError } = useGetAdminsQuery();
+
+  return (
+    <Box m='1.5rem 2.5rem'>
+      <Header title='Admins' subtitle='Managing admins and list of admins' />
+      {data ? (
+        <Box
+          mt={3}
+          height='75vh'
+          sx={{
+            '& .MuiDataGrid-root': {
+              border: 'none',
+            },
+            '& .MuiDataGrid-cell': {
+              borderBottom: 'none',
+            },
+            '& .MuiDataGrid-columnHeaders': {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: 'none',
+            },
+            '& .MuiDataGrid-virtualScroller': {
+              backgroundColor: theme.palette.primary.light,
+            },
+            '& .MuiDataGrid-footerContainer': {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: 'none',
+            },
+            '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            loading={isLoading}
+            getRowId={(row) => row._id}
+            rows={data?.data || []}
+            columns={columns}
+            components={{
+              ColumnMenu: CustomColumnMenu,
+            }}
+          />
+        </Box>
+      ) : isLoading ? (
+        <Loader mt={5} />
+      ) : isError ? (
+        <Alert
+          variant='outlined'
+          mt={4}
+          message={'An error ocurred. Please try again later.'}
+        />
+      ) : null}
+    </Box>
+  );
+};
+
+export default Admin;
