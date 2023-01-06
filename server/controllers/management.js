6d62fc2427511a31fcef20e1ac4dd0d9b25bd87b
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-06 20:48:12 +0530

    Add route and controller to get user performance

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,4 +1,6 @@
+import mongoose from 'mongoose';
 import User from '../models/User.js';
+import Transaction from '../models/Transaction.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
 
 export const getAdmins = asyncHandler(async (req, res, next) => {
@@ -11,5 +13,43 @@ export const getAdmins = asyncHandler(async (req, res, next) => {
     status: 200,
     count: admins.length,
     data: admins,
+  });
+});
+
+export const getPerformanceByUser = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const userWithStats = await User.aggregate([
+    { $match: { _id: new mongoose.Types.ObjectId(id) } },
+    {
+      $lookup: {
+        from: 'affiliatestats',
+        localField: '_id',
+        foreignField: 'userId',
+        as: 'affiliateStats',
+      },
+    },
+    { $unwind: '$affiliateStats' },
+  ]);
+
+  console.log(userWithStats);
+
+  const saleTransactions = await Promise.all(
+    userWithStats[0].affiliateStats.affiliateSales.map((id) => {
+      return Transaction.findById(id);
+    })
+  );
+
+  const filteredSaleTransactions = saleTransactions.filter(
+    (transaction) => transaction !== null
+  );
+
+  res.status(200).json({
+    success: true,
+    status: 200,
+    data: {
+      user: userWithStats[0],
+      sales: filteredSaleTransactions,
+    },
   });
 });
--- server/routes/management.js
+++ server/routes/management.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getAdmins } from '../controllers/management.js';
+import validateId from '../middlewares/validateId.js';
+import { getAdmins, getPerformanceByUser } from '../controllers/management.js';
 
 const router = express.Router();
 
 router.get('/admins', getAdmins);
+router.get('/performance/:id', validateId('id'), getPerformanceByUser);
 
 export default router;
