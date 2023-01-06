7b2c85c58d9daf3b9f71ae551d63d7e9e94e6794
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-06 22:12:17 +0530

    Add route and its controller to get dashboard data

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,6 @@
 import User from '../models/User.js';
+import OverallStat from '../models/OverallStat.js';
+import Transaction from '../models/Transaction.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
 
 export const getUser = asyncHandler(async (req, res, next) => {
@@ -9,5 +11,51 @@ export const getUser = asyncHandler(async (req, res, next) => {
     success: true,
     status: 200,
     data: user,
+  });
+});
+
+export const getDashboardStats = asyncHandler(async (req, res, next) => {
+  // Hardcoded values due to limited data availability.
+  const currentMonth = 'November';
+  const currentYear = 2021;
+  const currentDay = '2021-11-15';
+
+  /* Recent Transactions */
+  const transactions = await Transaction.find()
+    .limit(50)
+    .sort({ createdAt: -1 });
+
+  /* Overall Stats */
+  const overallStat = await OverallStat.find({ year: currentYear });
+
+  const {
+    totalCustomers,
+    yearlyTotalSoldUnits,
+    yearlySalesTotal,
+    monthlyData,
+    salesByCategory,
+  } = overallStat[0];
+
+  const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+    return month === currentMonth;
+  });
+
+  const todayStats = overallStat[0].dailyData.find(({ date }) => {
+    return date === currentDay;
+  });
+
+  res.status(200).json({
+    success: true,
+    status: 200,
+    data: {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions,
+    },
   });
 });
--- server/routes/general.js
+++ server/routes/general.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import validateId from '../middlewares/validateId.js';
-import { getUser } from '../controllers/general.js';
+import { getUser, getDashboardStats } from '../controllers/general.js';
 
 const router = express.Router();
 
 router.get('/user/:id', validateId('id'), getUser);
+router.get('/dashboard', getDashboardStats);
 
 export default router;
